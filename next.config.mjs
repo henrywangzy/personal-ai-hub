bc849a48fa38a33ddf0ffa4b12a16096558bea62
henrywangzy <zhengyuanwang@outlook.com>
2026-01-17 19:25:55 +0800

    feat: 添加微信公众号真实二维码

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'obsidian-remote-1331395796.cos.ap-guangzhou.myqcloud.com',
+      },
+    ],
   },
 };
 
--- src/config/site.ts
+++ src/config/site.ts
@@ -6,9 +6,8 @@ export const siteConfig: SiteConfig = {
   description: "泡泡妈，AI教育探索者，致力于把AI变成孩子的学习伙伴，变成自己的生活搭档",
   avatar: "/avatar.svg",
   social: {
-    wechat: "/qrcode-wechat.png",
+    wechat: "https://obsidian-remote-1331395796.cos.ap-guangzhou.myqcloud.com/henrywang-1331395796/%E5%9B%BE%E7%89%87%E9%BB%98%E8%AE%A4%E5%AD%98%E5%82%A8/%E4%BA%8C%E7%BB%B4%E7%A0%81.jpg",
     zhishixingqiu: "/qrcode-zsxq.png",
-    // github和email暂时移除，因为是虚构的
   },
 };
 
